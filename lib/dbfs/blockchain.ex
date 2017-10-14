44e88d9c31e4a75db7d996e4c72162aa4c9fa58f
Sheharyar Naseer <hello@sheharyar.me>
2017-10-13 20:37:31 -0400

    Blockchain module

---
+++ lib/dbfs/blockchain.ex
@@ -0,0 +1,23 @@
+defmodule DBFS.Blockchain do
+  alias DBFS.Block
+  alias DBFS.Blockchain
+
+  @moduledoc """
+  A naive Blockchain data structure implemented on top
+  of a Linked List
+  """
+
+  defstruct [:chain, :count]
+
+
+  def new do
+    %@module{chain: [Block.zero()], count: 1}
+  end
+
+
+  @doc "Get the newest block on the chain"
+  def head(%Blockchain{chain: [head | _tail]}) do
+    head
+  end
+
+end
