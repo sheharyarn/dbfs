6985bf3850eca64365701a22adb01723aa6710ba
Sheharyar Naseer <hello@sheharyar.me>
2017-11-18 23:00:13 -0500

    Database structure and migration

---
+++ priv/repo/migrations/20171128035202_create_blockchain.exs
@@ -0,0 +1,23 @@
+defmodule DBFS.Repo.Migrations.CreateBlockchain do
+  use Ecto.Migration
+
+  def change do
+    create table(:blockchain) do
+      add :data, :map
+    end
+
+    create table(:blocks) do
+      add :timestamp, :naive_datetime, null: false
+      add :type,      :integer,        null: false
+      add :prev,      :string,         null: false
+      add :hash,      :string,         null: false
+      add :signature, :string,         null: false
+      add :creator,   :string,         null: false
+      add :data,      :map,            null: false, default: "{}"
+    end
+
+    create index(:blocks, [:hash])
+    create index(:blocks, [:prev])
+    create index(:blocks, [:creator])
+  end
+end
