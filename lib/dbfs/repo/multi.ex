22f45316f772b45244c5927969d2fbdd94b4bcfc
Sheharyar Naseer <hello@sheharyar.me>
2017-11-25 17:24:57 -0500

    Repo.Multi set up

---
+++ lib/dbfs/repo/multi.ex
@@ -0,0 +1,28 @@
+defmodule DBFS.Repo.Multi do
+  @moduledoc "Helper Methods/Macros for Ecto.Multi operations"
+
+
+  @doc """
+  Macro to reduce Multi.run clutter without exposing
+  helper methods as public functions
+  """
+  defmacro run_operation(multi, name, args) when is_atom(name) do
+    quote do
+      name  = unquote(name)
+      args  = unquote(args)
+      multi = unquote(multi)
+
+      Ecto.Multi.run(multi, name, &( unquote(name)(&1, args) ))
+    end
+  end
+
+
+
+  @doc """
+  Normalize a Repo.transaction response into simple
+  2-item success/error tuple responses
+  """
+  def normalize({:ok, result}),         do: {:ok, result}
+  def normalize({:error, value}),       do: {:error, value}
+  def normalize({:error, _, value, _}), do: {:error, value}
+end
