06b34eb9b564f98abb7b39fe6154d73d4150cba4
Sheharyar Naseer <hello@sheharyar.me>
2018-03-20 14:05:13 -0400

    Election module for electing leader

---
+++ lib/dbfs/consensus/election.ex
@@ -0,0 +1,46 @@
+defmodule DBFS.Consensus.Election do
+  alias DBFS.Consensus
+
+  def get do
+    Consensus.Global.get
+  end
+
+
+  def get! do
+    %{state: state, leader: leader} = get()
+
+    case state do
+      :elected ->
+        leader
+
+      :electing ->
+        Process.sleep(100)
+        get!()
+    end
+  end
+
+
+  defp set!(state, leader \\ nil) do
+    Consensus.Global.set(%{state: state, leader: leader})
+  end
+
+
+  defp choose_leader! do
+    set!(:electing)
+    network = Consensus.Cluster.network_state
+
+    # TODO:
+    # Choosing the one with highest count for now.
+    # In future also match hashes to revert back
+    # state and perform corrections.
+
+    leader =
+      network
+      |> Map.get(:statuses)
+      |> Enum.find(&(network.max == &1.count))
+      |> Map.get(:name)
+
+    set!(:elected, leader)
+  end
+
+end
