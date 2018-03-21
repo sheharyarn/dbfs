310c6e62d5b7dd7ba27c9f297d514f51d1af28bd
Sheharyar Naseer <hello@sheharyar.me>
2018-03-21 14:08:49 -0400

    Electing leader + syncing

--- lib/dbfs/consensus/cluster.ex
+++ lib/dbfs/consensus/cluster.ex
@@ -7,6 +7,33 @@ defmodule DBFS.Consensus.Cluster do
 
   @doc "Synchronize with the network"
   def sync do
+    leader = Consensus.Election.ensure_leader!
+
+    if !Consensus.Node.self?(leader) do
+      leader = Consensus.Node.status(leader)
+      update_until_synced(leader)
+    end
+  end
+
+  defp update_until_synced(leader) do
+    current = Consensus.Node.status
+
+    cond do
+      current.count < leader.count ->
+        Logger.debug("#{node()} - Count Less than Leader")
+        sync_next(leader.name, current.last_hash)
+        update_until_synced(leader)
+
+      true ->
+        Logger.debug("#{node()} - Stopping sync")
+        :ok
+    end
+  end
+
+
+  defp sync_next(node, hash) do
+    %{block: block, file: file} = :rpc.call(node, Block.File, :next, [hash])
+    Blockchain.insert(Map.from_struct(block), file)
   end
 
 
