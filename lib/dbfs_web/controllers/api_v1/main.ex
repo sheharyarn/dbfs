6e1819576cc0a8f592cd0569623c3700afe0e0a6
Sheharyar Naseer <hello@sheharyar.me>
2018-03-29 15:18:22 -0400

    Serving network data using Consensus module

--- lib/dbfs_web/controllers/api_v1/main.ex
+++ lib/dbfs_web/controllers/api_v1/main.ex
@@ -5,7 +5,7 @@ defmodule DBFS.Web.Controllers.API.V1.Main do
   @doc "GET: Application Status"
   def index(conn, _params) do
     status = DBFS.Blockchain.status
-    nodes  = DBFS.Node.cluster_status
+    nodes  = DBFS.Consensus.Cluster.status
 
     conn
     |> determine_code(status)
@@ -16,7 +16,7 @@ defmodule DBFS.Web.Controllers.API.V1.Main do
 
   @doc "GET: Node Status"
   def nodes(conn, _params) do
-    nodes = DBFS.Node.cluster_status
+    nodes = DBFS.Consensus.status
     count = DBFS.Blockchain.status.count
 
     render(conn, :nodes, nodes: nodes, block_count: count)
